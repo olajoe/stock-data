cf12ea8bf1634719971ff82d6afbfc077fc1e6e9
olajoe <giorojoe@gmailcom>
2018-07-31 17:27:35 +0700

    fix test

--- src/__tests__/CompanyDetail.test.js
+++ src/__tests__/CompanyDetail.test.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { expect } from "jest";
 import { CompanyDetail } from "../components/CompanyDetail";
 
 describe("<CompanyDetail />", () => {
-  it.skip("should render without throwing an error", () => {
-
-    const wrapper = shallow(
-      <CompanyDetail
-        companyName= 'test'
-        symbol= 'test'
-        exchange= 'test'
-        description= 'test'
-        website= 'test'
-      />
-    )
-    expect(wrapper.find("h1")).toHaveLength(1);
+  it("should render without throwing an error", () => {
+    const props = {
+      companyName: 'test',
+      symbol: 'test',
+      exchange: 'test',
+      description: 'test',
+      website: 'test'
+    }
+  
+    const wrapper = shallow(<CompanyDetail{...props}/>)
+    expect(wrapper.find('h1')).toHaveLength(1);
+    expect(wrapper.find("h3")).toHaveLength(2);
   });
 
 });
